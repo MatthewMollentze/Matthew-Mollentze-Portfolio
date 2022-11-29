d19aee1f9ff82a8a644c1a6b9e505c64e28caabd
Matthew Mollentze <35138850+MatthewMollentze@users.noreply.github.com>
2022-11-29 22:47:53 +0200

    first public push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  pathPrefix: "/Matthew-Mollentze-Portfolio",
   plugins: [
     {
       resolve: "gatsby-theme-portfolio-minimal",
---
+++ public/component---src-pages-privacy-js-0ec284591b25049a4e16.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[844],{8897:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),a=n(8454);function l(){return r.createElement(r.Fragment,null,r.createElement(a.pQ,{title:"Privacy Policy",useTitleTemplate:!0,noIndex:!0}),r.createElement(a.T3,null,r.createElement(a.Ek,{sectionId:"privacy",heading:"Privacy Policy"})))}}}]);
+//# sourceMappingURL=component---src-pages-privacy-js-0ec284591b25049a4e16.js.map
